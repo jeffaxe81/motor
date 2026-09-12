0984eebf30d7f9b8b2e9ef5c0e366d34144f03a7
jeffaxe81 <jeffersonmachado81@gmail.com>
2026-09-12 11:26:09 -0300

    feat: exportar núcleo público M1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export {};
+export { AssetService } from "./application/assetService.js";
+export type { AssetRepository, AssetUpdateResult } from "./application/assetRepository.js";
+export type {
+  Asset,
+  AssetAuditEntry,
+  AssetCreateInput,
+  AssetRequestContext,
+  AssetUpdateInput,
+} from "./domain/asset.js";
+export { AssetError } from "./domain/asset.js";
+export { buildAssetApp } from "./http/app.js";
+export type { AssetContextResolver, BuildAssetAppOptions } from "./http/app.js";
+export { PostgresAssetRepository } from "./persistence/postgresAssetRepository.js";
